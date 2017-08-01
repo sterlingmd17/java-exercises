2026addff9205e1a613cb8d167891c085fbf7c7e
Sterling <sterlingmd17@gmail.com>
2017-07-31 17:16:35 -0700

    created class alice modified helloworld class

--- src/org/launchcode/java/demos/java4python/ElseIf.java
+++ src/org/launchcode/java/demos/java4python/ElseIf.java
@@ -7,19 +7,19 @@ import java.util.Scanner;
 public class ElseIf {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        System.out.println('enter a grade');
+        System.out.println("enter a grade");
         int grade = in.nextInt();
         
         if (grade < 60) {
-            System.out.println('F');
+            System.out.println("F");
         } else if (grade < 70) {
-            System.out.println('D');
+            System.out.println("D");
         } else if (grade < 80) {
-            System.out.println('C');
+            System.out.println("C");
         } else if (grade < 90) {
-            System.out.println('B');
+            System.out.println("B");
         } else {
-            System.out.println('A');
+            System.out.println("A");
         }
     }
 }
--- src/org/launchcode/java/demos/java4python/Hello.java
+++ src/org/launchcode/java/demos/java4python/Hello.java
@@ -1,10 +1,15 @@
 package org.launchcode.java.demos.java4python;
 
+import java.util.Scanner;
+
 /**
  * From "Java for Python Programmers"
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Whats your name?");
+        String name = in.next();
+        System.out.println("Hello,"+name);
     }
 }
